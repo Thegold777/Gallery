5d1ccfa915ce23987f9b5599c8778dccdc1c9241
Thegold777 <165582387+Thegold777@users.noreply.github.com>
2024-05-27 21:32:51 +0300

    .

--- index.js
+++ index.js
@@ -19,15 +19,15 @@ function handleClick() {
 
 function loadData() {
     return new Promise((resolve, reject) => {
-      // setTimeout не является частью решения
-      // Код ниже должен быть заменен на логику подходящую для решения вашей задачи
-      setTimeout(resolve, 2000);
+        // setTimeout не является частью решения
+        // Код ниже должен быть заменен на логику подходящую для решения вашей задачи
+        setTimeout(resolve, 2000);
     })
-  }
-  
-  loadData()
+}
+
+loadData()
     .then(() => {
-      let preloaderEl = document.getElementById('preloader');
-      preloaderEl.classList.add('hidden');
-      preloaderEl.classList.remove('visible');
+        let preloaderEl = document.getElementById('preloader');
+        preloaderEl.classList.add('hidden');
+        preloaderEl.classList.remove('visible');
     });
