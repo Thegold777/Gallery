373bf6793a4cce44307ee73debef2a83379080cf
Thegold777 <165582387+Thegold777@users.noreply.github.com>
2024-05-28 10:21:20 +0300

    ..

--- index.js
+++ index.js
@@ -1,42 +1,27 @@
 const loadBtn = document.querySelector(".load-btn");
-//const cleanBtn = document.querySelector(".clean-btn");
 const postsContainer = document.getElementById("posts-container");
+const loader = document.querySelector(".mask");
 
 function handleClick() {
-
+    loader.classList.remove('hidden');
     fetch("https://dog.ceo/api/breeds/image/random/20").then((response) => response.json())
         .then((data) => {
             console.log(data);
+            
             data.message.forEach((post) => {
                 const postElement = document.createElement("div");
                 postElement.classList.add("post");
                 postElement.innerHTML = `<img src="${post}">`;
                 postsContainer.appendChild(postElement);
             });
+        })
+        .catch(error => console.log(error))
+        .finally(() => {
+            setTimeout(() => {
+                loader.classList.add('hidden')
+            }, 2000);
+           
         });
+
 }
-
-//function loadData() {
-//   return new Promise((resolve, reject) => {
-// setTimeout не является частью решения
-// Код ниже должен быть заменен на логику подходящую для решения вашей задачи
-//       setTimeout(resolve, 2000);
-//    })
-//}
-
-//loadData()
-//    .then(() => {
-//       let preloaderEl = document.getElementById('preloader');
-//       preloaderEl.classList.add('hidden');
-//       preloaderEl.classList.remove('visible');
-//   });
-
-let mask = document.querySelector('.mask');
-window.addEventListener('load', () => {
-    mask.classList.add('hide');
-    setTimeout(() => {
-        mask.remove();
-    }, 600);
-
-});
 
